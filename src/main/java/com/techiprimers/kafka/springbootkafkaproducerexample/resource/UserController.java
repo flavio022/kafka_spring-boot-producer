8cb75757a07850fddf123220b8bf0ccdcdaf62bb
Flavio Danilo Silva da Costa <flavio@MacBook-Air-de-Flavio.local>
2021-04-06 14:11:01 -0300

    Created producer

---
+++ src/main/java/com/techiprimers/kafka/springbootkafkaproducerexample/resource/UserController.java
@@ -0,0 +1,24 @@
+package com.techiprimers.kafka.springbootkafkaproducerexample.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserController {
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+    private static final String TOPIC = "kafka_example";
+
+    @GetMapping("publish/{message}")
+    public String post(@PathVariable("message") final String message){
+
+        kafkaTemplate.send(TOPIC,message);
+
+        return "Published sucessfuly";
+    }
+}
